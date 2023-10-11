0bc6d0894186bf5e71a566422a1723047eb26d95
ehmaddd <ehmaddd@gmail.com>
2023-10-11 22:11:03 +0500

    Check for appropriate numRows

--- Solution.js
+++ Solution.js
@@ -1,3 +1,10 @@
 var convert = function(s, numRows) {
-
+    if (numRows === 1 || numRows >= s.length) {
+        return s;
+      }
 };
+
+const s = "PAYPALISHIRING";
+const numRows = 4;
+const result = convert(s, numRows);
+console.log(result);
