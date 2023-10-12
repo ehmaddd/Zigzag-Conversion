a52045856d84cdfaa06540953601528e96b364c4
ehmaddd <ehmaddd@gmail.com>
2023-10-12 20:44:03 +0500

    Add solution

--- Solution.js
+++ Solution.js
@@ -1,8 +1,26 @@
-var convert = function(s, numRows) {
-    if (numRows === 1 || numRows >= s.length) {
-        return s;
-      }
-};
+function convert(s, numRows) {
+  if (numRows === 1 || numRows >= s.length) {
+    return s;
+  }
+
+  const rows = new Array(numRows).fill('');
+  let currentRow = 0;
+  let isMovingDown = true;
+
+  for (const char of s) {
+    rows[currentRow] += char;
+
+    if (currentRow === 0) {
+      isMovingDown = true;
+    } else if (currentRow === numRows - 1) {
+      isMovingDown = false;
+    }
+
+    currentRow += isMovingDown ? 1 : -1;
+  }
+
+  return rows.join('');
+}
 
 const s = "PAYPALISHIRING";
 const numRows = 4;
